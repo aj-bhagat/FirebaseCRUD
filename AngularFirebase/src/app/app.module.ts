bc6b065b6cf95e0a88ee7ff40cd901203c081abf
aj-bhagat <ajaybhagatm620@gmail.com>
2019-10-03 17:06:27 +0530

    Firebase setup

--- AngularFirebase/src/app/app.module.ts
+++ AngularFirebase/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {AngularFireModule} from '@angular/fire';
-import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
