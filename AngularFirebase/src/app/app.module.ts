c42f836117007da8d4ee798f66d1fd3d92036fd7
aj-bhagat <ajaybhagatm620@gmail.com>
2019-10-04 15:03:03 +0530

    Component

--- AngularFirebase/src/app/app.module.ts
+++ AngularFirebase/src/app/app.module.ts
@@ -5,11 +5,13 @@ import {AngularFirestoreModule} from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
