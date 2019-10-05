18d3a916e91e21c8940770ee931a6b54cb492334
aj-bhagat <ajaybhagatm620@gmail.com>
2019-10-05 17:30:08 +0530

    FireBaseCRUD

--- AngularFirebase/src/app/app.module.ts
+++ AngularFirebase/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
+import {EmployeeService} from './shared/employee.service';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,7 @@ import { EmployeeComponent } from './employees/employee/employee.component';
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
