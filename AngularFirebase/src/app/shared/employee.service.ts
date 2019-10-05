b93b7bbba0006b395cb6455f7b5a30c894d2047f
aj-bhagat <ajaybhagatm620@gmail.com>
2019-10-05 17:31:22 +0530

    FireBaseCRUD

--- AngularFirebase/src/app/shared/employee.service.ts
+++ AngularFirebase/src/app/shared/employee.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-
+import {Employee} from './employee.model';
+import { FormsModule } from "@angular/forms";
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-
+  formData: Employee;
   constructor() { }
 }
