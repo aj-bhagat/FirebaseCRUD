d848156cf6135051d3ed7aa6f68e60f035252291
aj-bhagat <ajaybhagatm620@gmail.com>
2019-10-08 23:38:47 +0530

    Component

--- AngularFirebase/src/app/employees/employee/employee.component.ts
+++ AngularFirebase/src/app/employees/employee/employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {EmployeeService} from 'src/app//shared/employee.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
@@ -11,6 +12,18 @@ export class EmployeeComponent implements OnInit {
   constructor(private service: EmployeeService) { }
 
   ngOnInit() {
+  }
+
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.resetForm();
+    this.service.formData = {
+      id: null,
+      fullName:'',
+      position:'',
+      empCode:'',
+      mobile:'',
+    }
   }
 
 }
