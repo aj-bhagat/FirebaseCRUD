65b5ee8a1078d25c608d0d89100c68f4d55e0a88
aj-bhagat <ajaybhagatm620@gmail.com>
2019-10-10 14:12:36 +0530

    Component

--- AngularFirebase/src/app/employees/employee/employee.component.ts
+++ AngularFirebase/src/app/employees/employee/employee.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {EmployeeService} from 'src/app//shared/employee.service';
 import { NgForm } from '@angular/forms';
-
+import {AngularFirestore} from '@angular/fire/firestore';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
@@ -9,7 +9,7 @@ import { NgForm } from '@angular/forms';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor(private service: EmployeeService) { }
+  constructor(private service: EmployeeService, private firestore: AngularFirestore) { }
 
   ngOnInit() {
     this.resetForm();
@@ -27,6 +27,15 @@ export class EmployeeComponent implements OnInit {
     }
   }
 
+  onSubmit(form: NgForm) {
+    let data = Object.assign({}, form.value);
+    delete data.id;
+    if (form.value.id == null)
+      this.firestore.collection('employees').add(data);
+    else
+      this.firestore.doc('employees/' + form.value.id).update(data);
+    this.resetForm(form);
+  }
 
 
 }
